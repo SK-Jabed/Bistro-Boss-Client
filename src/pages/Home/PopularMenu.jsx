3a23884431e3be548b450624c38525b73adffd07
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-28 23:55:31 +0600

    Menu Items Fetched from Menu JSON Data

--- src/pages/Home/PopularMenu.jsx
+++ src/pages/Home/PopularMenu.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import SectionTitle from '../../components/SectionTitle';
 
 const PopularMenu = () => {
+        const [menu, setMenu] = useState([]);
+        useEffect(() => {
+          fetch("/menu.json")
+            .then((res) => res.json())
+            .then((data) => {
+              const popularItems = data.filter(
+                (item) => item.category === "popular"
+              );
+              setMenu(popularItems);
+            });
+        }, []);
     return (
       <section>
         <SectionTitle
