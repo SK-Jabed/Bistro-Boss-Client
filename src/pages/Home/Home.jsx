e9f8a095ded260e8eaecfc38a41a05acc6846b38
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-28 23:51:44 +0600

    Popular Menu Page Added and JSON Data Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Banner from './Banner';
 import CategorySwiper from './CategorySwiper';
+import PopularMenu from './PopularMenu';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <div className='container w-10/12 mx-auto'>
-
            <CategorySwiper></CategorySwiper>
+            <PopularMenu></PopularMenu>
            </div>
         </div>
     );
---
+++ src/pages/Home/PopularMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTitle from '../../components/SectionTitle';
+
+const PopularMenu = () => {
+    return (
+      <section>
+        <SectionTitle
+          heading="FROM OUR MENU"
+          subHeading="Check it out"
+        ></SectionTitle>
+      </section>
+    );
+};
+
+export default PopularMenu;
