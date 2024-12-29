6a25dccc7b6f71a6b80cd19402253ccb33974690
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-29 23:01:35 +0600

    Featured Item Page Added to the Project

---
+++ src/pages/Home/FeaturedItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SectionTitle from '../../components/SectionTitle';
+
+const FeaturedItem = () => {
+    return (
+        <div>
+            <SectionTitle subHeading={"check it out"} heading={"Featured Item"}></SectionTitle>
+            <div>
+                <div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedItem;
