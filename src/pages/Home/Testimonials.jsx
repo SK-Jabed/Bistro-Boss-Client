d5880a719a7adf93ba121bd12473e7aba946fa59
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-30 18:25:11 +0600

    Testimonials Section's Rating Added and Design Completed

--- src/pages/Home/Testimonials.jsx
+++ src/pages/Home/Testimonials.jsx
@@ -19,6 +19,10 @@ import "swiper/css/navigation";
 // import required modules
 import { Navigation } from "swiper/modules";
 import SectionTitle from "../../components/SectionTitle";
+import { Rating } from "@smastrom/react-rating";
+import { FaQuoteLeft } from "react-icons/fa";
+
+import "@smastrom/react-rating/style.css";
 
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
@@ -30,7 +34,7 @@ const Testimonials = () => {
   }, []);
 
   return (
-    <section className="mb-28">
+    <section className="mb-20">
       <SectionTitle
         subHeading={"What Our Clients Say"}
         heading={"TESTIMONIALS"}
@@ -39,9 +43,15 @@ const Testimonials = () => {
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
-            <div>
+            <div className="flex flex-col items-center mx-28 pb-16">
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <FaQuoteLeft className="text-6xl my-6" />
               <p>{review.details}</p>
-              <h3 className="text-2xl text-orange-500">{review.name}</h3>
+              <h3 className="text-2xl text-orange-500 mt-2">{review.name}</h3>
             </div>
           </SwiperSlide>
         ))}
