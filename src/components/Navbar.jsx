b740579dda9c32c7829b9ca99f52b3be5a45be05
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-31 23:55:15 +0600

    Menu JSON and Reviews JSON File Data Added to Database

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,69 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
- const navOptions = (
-   <>
-     <li>
-       <NavLink to={"/"}>HOME</NavLink>
-     </li>
-     <li>
-       <a>CONTACT US</a>
-     </li>
-     <li>
-       <a>DASHBOARD</a>
-     </li>
-     <li>
-       <NavLink to={"/menu"}>OUR MENU</NavLink>
-     </li>
-     <li>
-       <NavLink to={"/order/desserts"}>ORDER NOW</NavLink>
-     </li>
-   </>
- );
+  const navOptions = (
+    <>
+      <li>
+        <NavLink to={"/"}>HOME</NavLink>
+      </li>
+      <li>
+        <a>CONTACT US</a>
+      </li>
+      <li>
+        <a>DASHBOARD</a>
+      </li>
+      <li>
+        <NavLink to={"/menu"}>OUR MENU</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/order/desserts"}>ORDER NOW</NavLink>
+      </li>
+    </>
+  );
 
-    return (
-      <div>
-        <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+  return (
+    <div>
+      <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                {navOptions}
-              </ul>
-            </div>
-            <div className='flex flex-col'>
-              <a className="text-2xl font-semibold uppercase">Bistro Boss</a>
-              <span className='text-2xl font-semibold uppercase'>Restaurant</span>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navOptions}
+            </ul>
           </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">{navOptions}</ul>
-          </div>
-          <div className="navbar-end">
-            <a className="btn">Get started</a>
+          <div className="flex flex-col">
+            <a className="text-2xl font-semibold uppercase">Bistro Boss</a>
+            <span className="text-2xl font-semibold uppercase">Restaurant</span>
           </div>
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Get started</a>
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Order/CategoryTabs.jsx
+++ src/pages/Order/CategoryTabs.jsx
@@ -1,45 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-import useMenu from '../../hooks/useMenu';
-import FoodTabPanel from './FoodTabPanel';
-import { useParams } from 'react-router-dom';
+import useMenu from "../../hooks/useMenu";
+import FoodTabPanel from "./FoodTabPanel";
+import { useParams } from "react-router-dom";
 
 const CategoryTabs = () => {
-    const categories = ["desserts", "pizza", "salads", "soups", "drinks"];
-    const { category } = useParams();
-    const initialIndex = categories.indexOf(category);
-    const [tabIndex, setTabIndex] = useState(initialIndex);
-        const [menu] = useMenu();
-        const desserts = menu.filter((item) => item.category === "dessert");
-        const salads = menu.filter((item) => item.category === "salad");
-        const pizzas = menu.filter((item) => item.category === "pizza");
-        const soups = menu.filter((item) => item.category === "soup");
-        const drinks = menu.filter((item) => item.category === "drinks");
-        const offers = menu.filter((item) => item.category === "offered");
+  const categories = ["desserts", "pizza", "salads", "soups", "drinks"];
+  const { category } = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const salads = menu.filter((item) => item.category === "salad");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const soups = menu.filter((item) => item.category === "soup");
+  const drinks = menu.filter((item) => item.category === "drinks");
+  const offers = menu.filter((item) => item.category === "offered");
 
-    return (
-      <div>
-        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-          <TabList>
-            <Tab>Desserts</Tab>
-            <Tab>Pizza</Tab>
-            <Tab>Salad</Tab>
-            <Tab>Soups</Tab>
-            <Tab>Drinks</Tab>
-            <Tab>Offers</Tab>
-          </TabList>
-          <TabPanel>
-            <FoodTabPanel items={desserts}></FoodTabPanel>
-          </TabPanel>
-          <TabPanel><FoodTabPanel items={pizzas}></FoodTabPanel></TabPanel>
-          <TabPanel><FoodTabPanel items={salads}></FoodTabPanel></TabPanel>
-          <TabPanel><FoodTabPanel items={soups}></FoodTabPanel></TabPanel>
-          <TabPanel><FoodTabPanel items={drinks}></FoodTabPanel></TabPanel>
-          <TabPanel><FoodTabPanel items={offers}></FoodTabPanel></TabPanel>
-        </Tabs>
-      </div>
-    );
+  return (
+    <div>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Desserts</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Salad</Tab>
+          <Tab>Soups</Tab>
+          <Tab>Drinks</Tab>
+          <Tab>Offers</Tab>
+        </TabList>
+        <TabPanel>
+          <FoodTabPanel items={desserts}></FoodTabPanel>
+        </TabPanel>
+        <TabPanel>
+          <FoodTabPanel items={pizzas}></FoodTabPanel>
+        </TabPanel>
+        <TabPanel>
+          <FoodTabPanel items={salads}></FoodTabPanel>
+        </TabPanel>
+        <TabPanel>
+          <FoodTabPanel items={soups}></FoodTabPanel>
+        </TabPanel>
+        <TabPanel>
+          <FoodTabPanel items={drinks}></FoodTabPanel>
+        </TabPanel>
+        <TabPanel>
+          <FoodTabPanel items={offers}></FoodTabPanel>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default CategoryTabs;
