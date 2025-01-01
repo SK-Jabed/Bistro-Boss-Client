177f720866a4664b0beb9830b2ae2e79c71fbc93
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-01 23:18:21 +0600

    Recapcha Added to the Login Page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
 
 const Login = () => {
     const handleLogin = (e) => {
