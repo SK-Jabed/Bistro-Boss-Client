3e46413c6fdbd474f74b4f02e4677bff2f891564
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-03 23:04:13 +0600

    Sign In Functionalities Completed

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import auth from '../firebase/firebase.config';
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
 
