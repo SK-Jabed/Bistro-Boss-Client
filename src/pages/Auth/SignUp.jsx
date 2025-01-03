71c89b5c8bb217df8ce7dcc821612b9dabad74ef
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-03 23:35:49 +0600

    React Hook Form Implementation and Validation Activities Done

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../contexts/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const {
@@ -20,7 +21,7 @@ const SignUp = () => {
       console.log(loggedUser);
       updateUserProfile(data.name, data.photoURL)
         .then(() => {
-          console.log("user profile info updated");
+          console.log("User profile info updated");
           reset();
           Swal.fire({
             position: "top-end",
@@ -58,14 +59,14 @@ const SignUp = () => {
                 </label>
                 <input
                   type="text"
-                  // {...register("name", { required: true })}
+                  {...register("name", { required: true })}
                   name="name"
                   placeholder="Name"
                   className="input input-bordered"
                 />
-                {/* {errors.name && (
-                    <span className="text-red-600">Name is required</span>
-                  )} */}
+                {errors.name && (
+                  <span className="text-red-600">Name is required</span>
+                )}
               </div>
               <div className="form-control">
                 <label className="label">
@@ -73,13 +74,13 @@ const SignUp = () => {
                 </label>
                 <input
                   type="text"
-                  // {...register("photoURL", { required: true })}
+                  {...register("photoURL", { required: true })}
                   placeholder="Photo URL"
                   className="input input-bordered"
                 />
-                {/* {errors.photoURL && (
-                    <span className="text-red-600">Photo URL is required</span>
-                  )} */}
+                {errors.photoURL && (
+                  <span className="text-red-600">Photo URL is required</span>
+                )}
               </div>
               <div className="form-control">
                 <label className="label">
@@ -87,14 +88,14 @@ const SignUp = () => {
                 </label>
                 <input
                   type="email"
-                  // {...register("email", { required: true })}
+                  {...register("email", { required: true })}
                   name="email"
                   placeholder="email"
                   className="input input-bordered"
                 />
-                {/* {errors.email && (
-                    <span className="text-red-600">Email is required</span>
-                  )} */}
+                {errors.email && (
+                  <span className="text-red-600">Email is required</span>
+                )}
               </div>
               <div className="form-control">
                 <label className="label">
@@ -102,35 +103,34 @@ const SignUp = () => {
                 </label>
                 <input
                   type="password"
-                  // {...register("password", {
-                  //   required: true,
-                  //   minLength: 6,
-                  //   maxLength: 20,
-                  //   pattern:
-                  //     /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
-                  // })}
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 16,
+                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
                   placeholder="password"
                   className="input input-bordered"
                 />
-                {/* {errors.password?.type === "required" && (
-                    <p className="text-red-600">Password is required</p>
-                  )}
-                  {errors.password?.type === "minLength" && (
-                    <p className="text-red-600">
-                      Password must be 6 characters
-                    </p>
-                  )}
-                  {errors.password?.type === "maxLength" && (
-                    <p className="text-red-600">
-                      Password must be less than 20 characters
-                    </p>
-                  )}
-                  {errors.password?.type === "pattern" && (
-                    <p className="text-red-600">
-                      Password must have one Uppercase one lower case, one
-                      number and one special character.
-                    </p>
-                  )} */}
+                {errors.password?.type === "required" && (
+                  <p className="text-red-600">Password is required</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-600">
+                    Password must be at least 6 characters
+                  </p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p className="text-red-600">
+                    Password must be less than 16 characters
+                  </p>
+                )}
+                {errors.password?.type === "pattern" && (
+                  <p className="text-red-600">
+                    Password must have one Uppercase one lower case, one number
+                    and one special character.
+                  </p>
+                )}
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">
                     Forgot password?
