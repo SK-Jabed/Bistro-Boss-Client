d958f5c8d8069cb2556a4707bec8ca21a8d2ccd3
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-08 02:17:58 +0600

    All Users Page's Tabular Design and Functionalities Done

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -6,7 +6,7 @@ import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
@@ -14,30 +14,30 @@ const AllUsers = () => {
     },
   });
 
-      const handleDeleteUser = (user) => {
-        Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            axiosSecure.delete(`/users/${user._id}`).then((res) => {
-              if (res.data.deletedCount > 0) {
-                refetch();
-                Swal.fire({
-                  title: "Deleted!",
-                  text: "User has been deleted.",
-                  icon: "success",
-                });
-              }
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "User has been deleted.",
+              icon: "success",
             });
           }
         });
-      };
+      }
+    });
+  };
 
   return (
     <div>
@@ -71,9 +71,7 @@ const AllUsers = () => {
                       onClick={() => handleMakeAdmin(user)}
                       className="btn btn-lg bg-orange-500"
                     >
-                      <FaUsers
-                        className="text-white text-2xl"
-                      ></FaUsers>
+                      <FaUsers className="text-white text-2xl"></FaUsers>
                     </button>
                   )}
                 </td>
