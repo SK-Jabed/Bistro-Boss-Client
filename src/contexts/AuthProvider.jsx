9be2a282360ccb2eb46076c8598e9ea6761f72e8
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-08 23:07:02 +0600

    Admin Route Created and JWT Token With LS Completed

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import auth from '../firebase/firebase.config';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import useAxiosPublic from '../hooks/useAxiosPublic';
 
 export const AuthContext = createContext(null);
 
@@ -8,6 +9,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
       setLoading(true);
@@ -39,13 +41,24 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
       const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
         setUser(currentUser);
-        console.log("Current User --->", currentUser);
+        if (currentUser) {
+          // get token and store client
+          const userInfo = { email: currentUser.email };
+          axiosPublic.post("/jwt", userInfo).then((res) => {
+            if (res.data.token) {
+              localStorage.setItem("access-token", res.data.token);
+            }
+          });
+        } else {
+          // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+          localStorage.removeItem("access-token");
+        }
         setLoading(false);
       });
       return () => {
         return unsubscribe();
       };
-    }, []);
+    }, [axiosPublic]);
 
     const authInfo = {
       user,
