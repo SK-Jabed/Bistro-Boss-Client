4c36ca6bfc1c64ad2c9cd03d82a5779cb1b95ab7
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-09 23:29:32 +0600

    File Upload Functionality with ImageBB Completed for Add Menu Items

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -12,6 +12,7 @@ const AddItems = () => {
   const { register, handleSubmit, reset } = useForm();
   const axiosPublic = useAxiosPublic();
   const axiosSecure = useAxiosSecure();
+  
   const onSubmit = async (data) => {
     console.log(data);
     // image upload to imgbb and then get an url
